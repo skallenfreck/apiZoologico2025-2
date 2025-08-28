fbf78be8dd41c69fc4903d618ea4f8f8ab19dbe9
skll <fresnedaalexander@gmail.com>
2025-08-27 22:11:12 -0500

    Se crea el serviodor node

---
+++ index.js
@@ -0,0 +1,12 @@
+        // Se carga el módulo de HTTP
+        var http = require("http");
+
+        // Se crea el servidor HTTP y se define la escucha en el puerto 8000
+        http.createServer(function (request, response) {
+          // Se define la cabecera HTTP: 200 (OK) y el tipo de contenido como texto plano
+          response.writeHead(200, { "Content-Type": "text/plain" });
+          // Se responde con el mensaje "Hola Mundo!"
+          response.end("Hola Mundo!\n");
+        }).listen(8000); // El servidor escuchará en el puerto 8000
+
+        console.log("Servidor iniciado en http://127.0.0.1:8000/");
